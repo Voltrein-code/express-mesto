7ac275ff65f8eef289d03b44728b4216604c71c9
Voltrein-code <mr-voltrein@yandex.ru>
2021-06-18 13:48:35 +0300

    feature: add logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { validateSignIn, validateSignUp, validateRoutesWithAuth } = require('./middlewares/request-validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
@@ -19,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(requestLogger);
+
 app.post('/signin', validateSignIn, login);
 app.post('/signup', validateSignUp, createUser);
 
@@ -26,6 +29,8 @@ app.use('/users', validateRoutesWithAuth, auth, require('./routes/users'));
 app.use('/cards', validateRoutesWithAuth, auth, require('./routes/cards'));
 
 app.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемый ресурс не найден')));
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use((err, req, res, next) => {
